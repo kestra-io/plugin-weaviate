28ff1397c421893d1755bb3b2bbb92414567ff2b
ben8t <pimpaudben@gmail.com>
2025-12-30 15:43:27 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/weaviate/package-info.java
+++ src/main/java/io/kestra/plugin/weaviate/package-info.java
@@ -1,7 +1,11 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Weaviate database.\n"+
         "Weaviate is an vector database. It allows you to store data objects and vector embeddings from your favorite ML-models",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    categories = PluginSubGroup.PluginCategory.DATABASE,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.AI
+    }
 )
 package io.kestra.plugin.weaviate;
 
